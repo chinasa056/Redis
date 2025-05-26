b968e829409fa12a35a21af9c8f08b88ff37191b
chinasa056 <chinasaacha05@gmail.com>
2025-05-26 21:19:03 +0100

    feat: configured the server

---
+++ Redis_With_OpenWeather_API/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT;
+
+const app = express()
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+    
+})
+
